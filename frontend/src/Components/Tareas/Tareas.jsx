a7f7a65106431119a77bd49f19b99320b8761a81
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-03-20 19:42:54 +0100

    Por fin operador ternario para estilos en tareas

--- frontend/src/Components/Tareas/Tareas.jsx
+++ frontend/src/Components/Tareas/Tareas.jsx
@@ -8,7 +8,7 @@ function Tareas({listaTareas}) {
     <ul className={styles.lista}>
       {listaTareas.map((tarea)=>{return(
       <li key={tarea.id}> <div className={styles.tarea}> <input type="checkbox" checked={tarea.rematada}/>
-      {tarea.descripcion} <span className={styles.rematadaOno}>{tarea.rematada ? " 😎 Feito" : " 😫 Pendente"} </span></div>
+      {tarea.descripcion} <span className={tarea.rematada ? styles.Feito : styles.Pendente}>{tarea.rematada ? " 😎 Feito" : " 😫 Pendente"} </span></div>
       </li>
       )})}
     </ul>
