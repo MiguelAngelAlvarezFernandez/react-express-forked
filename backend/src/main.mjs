47daec7c6261848852bec6b467c73f4f6b6245bd
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-03-23 20:03:48 +0100

    Incorporados PUT y DELETE

--- backend/src/main.mjs
+++ backend/src/main.mjs
@@ -34,6 +34,39 @@ app.get("/tarefa/", (_, respuesta)=>{
     respuesta.send(JSON.stringify(tarefas))
 })
 
+app.put("/tarefa/", (peticion, respuesta)=>{
+const indice = tarefas.findIndex (
+    function (tarefa) {
+        return peticion.body.id === tarefa.id
+    })
+/*devuelve el índice del primer elemento de un array que cumpla 
+con la función de prueba proporcionada. 
+En caso contrario devuelve -1 */
+if (indice===-1){
+respuesta.status(404)
+respuesta.send("Tarefa not found")
+return /* El return "vacio" se pone para que finalice aquí la función 
+          sin leer el resto en caso de que se cumpla la condición*/
+} else {tarefas.splice(indice, 1, peticion.body)
+respuesta.status(200)
+respuesta.send("Ok")}
+})
+
+app.delete("/tarefa/", (peticion, respuesta)=>{
+const indice = tarefas.findIndex (
+function (tarefa) {
+return peticion.body.id === tarefa.id
+})
+if (indice===-1){
+respuesta.status(404)
+respuesta.send("Tarefa not found")
+return 
+} else {tarefas.splice(indice, 1)
+respuesta.status(200)
+respuesta.send("Ok")}
+})
+
+
 app.listen( 8000,()=>{
     console.log("Express traballando...");
 })
