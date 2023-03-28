0c09efd917a66dfd2f47c94a5924df2c33f36416
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-03-28 18:33:13 +0200

    Incluido post para insertar datos nuevos en BBDD

--- backend/src/main.mjs
+++ backend/src/main.mjs
@@ -155,56 +155,23 @@ app.get("/tarefa/", (peticion, respuesta)=>{
 }
 )
 
-
-
-/*app.get("/", (_, respuesta)=>{
-    respuesta.status(200)
-    respuesta.send("¡¡¡Funciono!!!")
-})*/
-
-/*app.post("/tarefa/", (peticion, respuesta)=>{
-    tarefas.push(peticion.body)
-    respuesta.status(200)
-    respuesta.send("Ok")
-})*/
-
-/*app.get("/tarefa/", (_, respuesta)=>{
-    respuesta.status(200)
-    respuesta.send(JSON.stringify(tarefas))
-})*/
-
 app.put("/tarefa/", (peticion, respuesta)=>{
-const indice = tarefas.findIndex (
-    function (tarefa) {
-        return peticion.body.id === tarefa.id
-    })
-/*devuelve el índice del primer elemento de un array que cumpla 
-con la función de prueba proporcionada. 
-En caso contrario devuelve -1 */
-if (indice===-1){
-respuesta.status(404)
-respuesta.send("Tarefa not found")
-return /* El return "vacio" se pone para que finalice aquí la función 
-          sin leer el resto en caso de que se cumpla la condición*/
-} else {tarefas.splice(indice, 1, peticion.body)
-respuesta.status(200)
-respuesta.send("Ok")}
+    db.run(
+        'UPDATE tareas SET descripcion = ?, rematada = ? WHERE id = ?',
+        [peticion.body.descripcion, peticion.body.rematada, peticion.body.id],
+        (error)=>{
+            if (error) {
+                console.error(error) 
+                respuesta.status(500)
+                respuesta.send(`Error accediendo a la base de datos.
+                Consulta la consola del backend para más información`)
+            } else {
+                respuesta.status(200)
+                respuesta.send("Ok")
+            }
+        }
+    )
 })
-
-/*app.delete("/tarefa/", (peticion, respuesta)=>{
-const indice = tarefas.findIndex (
-function (tarefa) {
-return peticion.body.id === tarefa.id
-})
-if (indice===-1){
-respuesta.status(404)
-respuesta.send("Tarefa not found")
-return 
-} else {tarefas.splice(indice, 1)
-respuesta.status(200)
-respuesta.send("Ok")}
-})*/
-
 
 app.listen( 8000,()=>{
     console.log("Express traballando...");
